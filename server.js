f46a1933dd7f7b00f76a8c8e6fab071dfc223483
omer biton <omerbiton@omers-MacBook-Pro.local>
2020-04-29 00:33:19 +0300

    cors fix18

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
-app.use(function (req, res, next) {
+app.all(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
