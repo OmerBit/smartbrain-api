fe3b5b63788faf4ae7e4838501bd64445cac45b2
omer biton <omerbiton@omers-MacBook-Pro.local>
2020-04-28 21:01:24 +0300

    cors fix2

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ console.log(
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
   next();
 });
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   // res.send(database.users);
