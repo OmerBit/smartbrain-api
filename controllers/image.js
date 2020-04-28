d7471a2a610a4b8cdcdcda9fbd0d10b90b347a93
omer biton <omerbiton@omers-MacBook-Pro.local>
2020-04-28 14:57:54 +0300

    deploy to heroku

---
+++ controllers/image.js
@@ -0,0 +1,40 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "23b52b8b6e254738b678e6275529e625",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json("unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+  // let found = false;
+  // database.users.forEach((user) => {
+  //   if (user.id === id) {
+  //     found = true;
+  //     user.entries++;
+  //     return res.json(user.entries);
+  //   }
+  // });
+  // if (!found) {
+  //   res.status(400).json("no such user");
+  // }
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall,
+};
