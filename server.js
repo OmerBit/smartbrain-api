24c9d0d3461e8f78193761e5fca9f785b925ad31
omer biton <omerbiton@omers-MacBook-Pro.local>
2020-04-29 00:20:29 +0300

    cors fix15

--- server.js
+++ server.js
@@ -35,10 +35,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://git.heroku.com/damp-garden-24642.git"
-  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
