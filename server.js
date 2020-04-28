3b3e1c9d439a82c53c2342b94398e455158ead7a
omer biton <omerbiton@omers-MacBook-Pro.local>
2020-04-28 21:44:00 +0300

    cors fix7

--- server.js
+++ server.js
@@ -32,10 +32,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://git.heroku.com/damp-garden-24642.git"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
